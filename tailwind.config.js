f04963c79555d938c6eaa263423b80e1d7b66319
mpdev2311 <marcospdev2311gmail.com>
2022-04-19 15:03:45 -0300

    arquivo configuração

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+
+module.exports = {
+  content: [],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          100: '#bc42f5',
+          200: '#702991',
+          300: '#3d164f',
+        }
+      }
+    },
+  },
+  varianrs: {
+     extend: {},
+  }, 
+  plugins: [],
+}
